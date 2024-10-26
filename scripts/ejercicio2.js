0b7ffc7b6e6189958d546f62d7385f01fc48a875
ManoloSanchezHernandez <2303084@utrivieramaya.edu.mx>
2024-10-25 22:20:55 -0500

    Ejercicio2 de JavaScript

---
+++ scripts/ejercicio2.js
@@ -0,0 +1,102 @@
+//primera instruccion 
+let challenge =("30 Days of JavaScript")
+
+
+//segunda instruccion
+console.log(challenge);
+
+
+//tercera instruccion
+console.log(challenge.length);
+
+
+//cuarta instruccion
+console.log(challenge.toUpperCase());
+
+
+//quinta instruccion
+console.log(challenge.toLowerCase());
+
+
+//sexta instruccion
+console.log(challenge.substring(3));
+
+
+//septima instruccion
+console.log(challenge.replace("Days of JavaScript", ""));
+
+
+//octava instruccion
+console.log(challenge.includes("Script"));
+
+
+//novena instruccion
+console.log(challenge.split(""));
+
+
+//decima instruccion
+let array = ("1 2 3 4 5");
+console.log(array.split(''));
+
+
+//undecima instruccion
+let companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(companies.split(", "));
+
+
+//dodecima instruccion
+console.log(challenge.replace("30 Days of JavaScript", "30 Days of Node"));
+
+
+//decima tercera instruccion
+console.log(challenge.charAt(15));
+
+
+//decima cuarta instruccion
+console.log(challenge.charCodeAt(11));
+
+
+//decima quinta instruccion
+console.log(challenge.indexOf('a'));
+
+
+//decima sexta instruccion
+console.log(challenge.lastIndexOf('a'));
+
+
+//decima septima instruccion
+let text = ("You cannot end a sentence with because because because is a conjunction");
+console.log(text.indexOf( 'because'));
+
+
+//decima octava instruccion
+console.log(text.lastIndexOf("because"));
+
+
+//decima novena instruccion
+console.log(text.search("because"));
+
+
+//vigesima instruccion
+let espacio = (" 30 Days Of JavaScript ");
+console.log(espacio.trim());
+
+
+//vigesima primera instruccion
+console.log(challenge.startsWith("30"));
+
+
+//vigesima segunda instruccion
+console.log(challenge.endsWith("JavaScript"));
+
+
+//vigesima tercera instruccion
+console.log(challenge.match("a"));
+
+
+//vigesima cuarta instruccion
+console.log(challenge.concat(" ","30 Days of","Script"));
+
+
+//vigesima quinta instruccion
+console.log(challenge.repeat(30));
